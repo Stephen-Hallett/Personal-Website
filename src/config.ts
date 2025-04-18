f3ebe5739b582223dc7983f8fae1b3a5caf3cb73
Stephen-Hallett <stevohallett@gmail.com>
2025-04-18 19:24:41 +1200

    feat: Create website from astrofy template

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Stephen Portfoio";
+export const SITE_DESCRIPTION =
+  "Hi, I'm Stephen - A data scientist based in Auckland, New Zealand. I have experience in computer vision, NLP, Docker and more. Check out my projects, research and running services for more info!";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
