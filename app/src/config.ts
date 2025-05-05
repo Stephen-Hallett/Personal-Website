d2ece6f68e5b76265f2fcbe3d6a33d1d3bbfdb80
Stephen-Hallett <stevohallett@gmail.com>
2025-05-05 18:10:36 +1200

    fix: Fix typo in site title

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Stephen Portfoio";
+export const SITE_TITLE = "Stephen Portfolio";
 export const SITE_DESCRIPTION =
   "Hi, I'm Stephen - A data scientist based in Auckland, New Zealand. I have experience in computer vision, NLP, Docker and more. Check out my projects, research and running services for more info!";
 export const GENERATE_SLUG_FROM_TITLE = true;
